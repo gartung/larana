46beaad4297a7f86ce0dee2eabab1f0370166699
Pawel Kryczynski <pkryczyn@lariatgpvm01.fnal.gov>
2014-07-04 14:40:23 -0500

    changes for lariat new optical simluation

--- OpticalDetector/SimPhotonCounter_module.cc
+++ OpticalDetector/SimPhotonCounter_module.cc
@@ -23,7 +23,7 @@
 // bool    MakeDetectedPhotonsTree
 // bool    MakeOpDetsTree
 // bool    MakeOpDetEventsTree
-// double  QantumEfficiency   - Quantum efficiency of OpDet
+// double  QuantumEfficiency   - Quantum efficiency of OpDet
 // double  WavelengthCutLow   - Sensitive wavelength range of OpDet
 // double  WavelengthCutHigh
 #ifndef SimPhotonCounter_h
@@ -57,6 +57,8 @@
 #include <TH1D.h>
 #include <TF1.h>
 #include <TTree.h>
+#include <TVectorT.h>
+#include <TAxis.h>
 
 // C++ language includes
 #include <iostream>
@@ -66,7 +68,8 @@
 
 #include "CLHEP/Random/RandFlat.h"
 #include "CLHEP/Random/RandGaussQ.h"
-
+int allphotons1=0;
+int allphotons2=0;
 namespace opdet {
 
   class SimPhotonCounter : public art::EDAnalyzer{
@@ -91,7 +94,7 @@ namespace opdet {
 
 
       // Parameters to read in
-
+	
       std::string fInputModule;      // Input tag for OpDet collection
 
       int fVerbosity;                // Level of output to write to std::out
@@ -99,10 +102,21 @@ namespace opdet {
       bool fMakeDetectedPhotonsTree; //
       bool fMakeAllPhotonsTree;      //
       bool fMakeOpDetsTree;         // Switches to turn on or off each output
-      bool fMakeOpDetEventsTree;          //
-      
-      float fQE;                     // Quantum efficiency of tube
-
+      bool fMakeOpDetEventsTree; 
+	bool fDetailedQE;         //
+      bool fFill;
+      bool fUseQE;
+      float fQE; 	
+	double fftest;              // Quantum efficiency of tube
+      std::vector<double> fQE2H; 
+      std::vector<double> fQE2E;
+      std::vector<double> fQE2Si;    
+      std::vector<double> fQEn;                   // Quantum efficiency of tube
+	//Histrograms to store efficiency
+	TH1D *QeEnergyHist;
+	TH1D *QEHmm;
+	TH1D *QEEtl;
+	TH1D *QESipm;
       float fWavelengthCutLow;       // Sensitive wavelength range 
       float fWavelengthCutHigh;      // 
 
@@ -142,13 +156,41 @@ namespace opdet {
     fMakeDetectedPhotonsTree=  pset.get<bool>("MakeDetectedPhotonsTree");
     fMakeOpDetsTree=           pset.get<bool>("MakeOpDetsTree");
     fMakeOpDetEventsTree=      pset.get<bool>("MakeOpDetEventsTree");
+    fDetailedQE=       pset.get<bool>("QuantumEfficiencyDetails");
+    fUseQE=       pset.get<bool>("UsePMTEff");
     fQE=                       pset.get<double>("QuantumEfficiency");
+	if(fDetailedQE){
+    fQE2H=                       pset.get< std::vector<double> >("QuantumEfficiencyVectorHmm");
+    fQE2E=                       pset.get< std::vector<double> >("QuantumEfficiencyVectorEtl");
+    fQE2Si=                       pset.get< std::vector<double> >("QuantumEfficiencyVectorSi"); 
+    fQEn=                       pset.get< std::vector<double> >("QuantumEfficiencyEnergies");
+	//Double_t qen[10];
+	TVectorT<double> qen2(10);
+	for(int i=0;i<int(fQEn.size());i++) qen2[i]=fQEn[i];
+	
+	
+
+	QeEnergyHist=new TH1D(qen2);
+	QEHmm=new TH1D(qen2);
+	QEEtl=new TH1D(qen2);
+	QESipm=new TH1D(qen2);
+
+	for(int j=0;j<10;j++) {
+	QEHmm->SetBinContent(j,fQE2H[j]);
+	QEEtl->SetBinContent(j,fQE2E[j]);
+	QESipm->SetBinContent(j,fQE2Si[j]);
+	
+	}
+
+	}
     fWavelengthCutLow=         pset.get<double>("WavelengthCutLow");
     fWavelengthCutHigh=        pset.get<double>("WavelengthCutHigh");
     // get the random number seed, use a random default if not specified    
     // in the configuration file.  
     unsigned int seed = pset.get< unsigned int >("Seed", sim::GetRandomNumberSeed());
-    createEngine(seed);    
+    createEngine(seed);   
+
+ 
   }
   
 
@@ -224,7 +266,12 @@ namespace opdet {
 
     art::ServiceHandle<sim::LArG4Parameters> lgp;
     bool fUseLitePhotons = lgp->UseLitePhotons();
-
+    //Reset counters
+    fCountEventAll=0;
+    fCountEventDetected=0;
+    //Reset counters
+   fCountOpDetAll=0;
+    fCountOpDetDetected=0;
     if(!fUseLitePhotons)
     {
     //Get SimPhotonsCollection from Event
@@ -233,7 +280,9 @@ namespace opdet {
     //Reset counters
     fCountEventAll=0;
     fCountEventDetected=0;
-
+    allphotons1=0;
+	    fCountOpDetAll=0;
+	    fCountOpDetDetected=0;
     if(fVerbosity > 0) std::cout<<"Found OpDet hit collection of size "<< TheHitCollection.size()<<std::endl;
     if(TheHitCollection.size()>0)
       {
@@ -242,86 +291,208 @@ namespace opdet {
 	    //Reset Counters
 	    fCountOpDetAll=0;
 	    fCountOpDetDetected=0;
+	    allphotons1=0;
 
 	    //Get data from HitCollection entry
 	    fOpChannel=itOpDet->first;
 	    const sim::SimPhotons& TheHit=itOpDet->second;
-	     
-	    //	    std::cout<<"OpDet " << fOpChannel << " has size " << TheHit.size()<<std::endl;
+	      
+	    	   // std::cout<<"Reseted counters, OpDet channel " << fOpChannel << " has size " << TheHit->size()<<std::endl;
 	    
 	    // Loop through OpDet phots.  
 	    //   Note we make the screen output decision outside the loop
 	    //   in order to avoid evaluating large numbers of unnecessary 
 	    //   if conditions. 
 
-	    if(fVerbosity > 3)
+	    if(fVerbosity > 3) 
 	      {
+
+
+		std::cout<<"was here 302================== verbosity >3 "<<std::endl;
 		for(const sim::OnePhoton& Phot: TheHit)
 		  {
+
+std::cout<<"detected photons reset test ---------------------- "<<fCountOpDetDetected<<" channel "<<fOpChannel<<std::endl;
+			allphotons1++;
+			allphotons2++;
 		    // Calculate wavelength in nm
 		    fWavelength= (2.0*3.142)*0.000197/Phot.Energy;
 
 		    //Get arrival time from phot
 		    fTime= Phot.Time;
-		    std::cout<<"Arrival time: " << fTime<<std::endl;
+			fCountOpDetAll++;
+		   // std::cout<<" 305 Arrival time: " << fTime<<std::endl;
 		    
 		    // Increment per OpDet counters and fill per phot trees
-		    fCountOpDetAll++;
+		fFill=false;
+std::cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% details of QE "<<fDetailedQE<<" "<<fWavelength<<std::endl;
+		 
 		    if(fMakeAllPhotonsTree) fThePhotonTreeAll->Fill();
 		    if((flat.fire(1.0)<=fQE)&&(fWavelength>fWavelengthCutLow)&&(fWavelength<fWavelengthCutHigh))
 		      {
-			if(fMakeDetectedPhotonsTree) fThePhotonTreeDetected->Fill();
-			fCountOpDetDetected++;
-			std::cout<<"OpDetResponse PerPhoton : Event "<<fEventID<<" OpChannel " <<fOpChannel << " Wavelength " << fWavelength << " Detected 1 "<<std::endl;
-		      }
-		    else
-		      std::cout<<"OpDetResponse PerPhoton : Event "<<fEventID<<" OpChannel " <<fOpChannel << " Wavelength " << fWavelength << " Detected 0 "<<std::endl;
-		  }
-	      }
+		
+		if(fUseQE){
+		std::cout<<"QE used 321!!!"<<fUseQE<<" wavelengh"<<fWavelengthCutLow<<" "<<fWavelength<<" "<<fWavelengthCutHigh<<std::endl;
+		if(fDetailedQE){
+			fFill=true;
+			switch(fOpChannel){
+				case 0:{
+				fftest=flat.fire(1.0);
+				  std::cout<<"channel 0 "<<QEHmm->GetBinCenter(1)<<" "<<fftest<<" "<<QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))<<std::endl;
+				if(fftest>QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) 					fFill=false;
+				break;
+				}
+				case 1:{
+				fftest=flat.fire(1.0);
+			//	  std::cout<<"channel 1"<<std::endl;
+				if(fftest>QEHmm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) 					fFill=false;
+				break;
+				}
+				case 2:{
+				fftest=flat.fire(1.0);
+			//	 std::cout<<"channel 2"<<std::endl;
+				if(fftest>QEEtl->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) 					fFill=false;
+				break;
+				}
+				case 3:{
+				fftest=flat.fire(1.0);
+			//	  std::cout<<"channel 3"<<std::endl;
+				if(fftest>QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) 					fFill=false;
+				break;
+				}
+				default:
+				std::cout<<"error"<<std::endl;
+			}
+
+		}//QE details
+	}//QE used
+		else{
+			fFill=true;// no QE
+			std::cout<<"QE not used 357!!!"<<fUseQE<<std::endl;
+				}
+			if(fFill==true) fCountOpDetDetected++;
+			if(fMakeDetectedPhotonsTree&&fFill==true) fThePhotonTreeDetected->Fill();
+				
+				
+			fFill=false;
+
+		      }//wavelength cut
+			
+			fFill=false;
+		  }//loop over simphotons
+	      }//verbosity >3
 	    else
 	      {
+    //Reset counters
+   fCountOpDetAll=0;
+    fCountOpDetDetected=0;
+
+std::cout<<"was here 381================== verbosity <3 "<<std::endl;
 		for(const sim::OnePhoton& Phot: TheHit)
 		  {
+			std::cout<<"detected photons reset test ---------------------- "<<fCountOpDetDetected<<" channel "<<fOpChannel<<std::endl;
+			allphotons1++;
+			allphotons2++;
 		    // Calculate wavelength in nm
 		    fWavelength= (2.0*3.142)*0.000197/Phot.Energy;
 		    fTime= Phot.Time;		
     
 		    // Increment per OpDet counters and fill per phot trees
-		    fCountOpDetAll++;
+		   fCountOpDetAll++;
+		    fFill=false;
 		    if(fMakeAllPhotonsTree) fThePhotonTreeAll->Fill();
 		    if((flat.fire(1.0)<=fQE)&&(fWavelength>fWavelengthCutLow)&&(fWavelength<fWavelengthCutHigh))
 		      {
-			if(fMakeDetectedPhotonsTree) fThePhotonTreeDetected->Fill();
-			fCountOpDetDetected++;
-		      }
-		  }
-	      }
+
+	
+		
+		if(fUseQE){
+
+		std::cout<<"QE used 394!!!"<<fUseQE<<std::endl;
+		if(fDetailedQE){
+			fFill=true;
+			switch(fOpChannel){
+				case 0:{
+				fftest=flat.fire(1.0);
+				std::cout<<"channel 0 "<<QEHmm->GetBinCenter(1)<<" "<<fftest<<" "<<QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))<<std::endl;
+				if(fftest>QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) fFill=false;
+				break;
+				}
+				case 1:{
+				fftest=flat.fire(1.0);
+				std::cout<<"channel 1"<<std::endl;
+				if(fftest>QEHmm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) fFill=false;
+				break;
+				}
+				case 2:{
+				fftest=flat.fire(1.0);
+				std::cout<<"channel 2"<<std::endl;
+				if(fftest>QEEtl->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) fFill=false;
+				break;
+				}
+				case 3:{
+				fftest=flat.fire(1.0);
+				std::cout<<"channel 3"<<std::endl;
+				if(fftest>QESipm->GetBinContent(QESipm->GetXaxis()->FindBin(Phot.Energy/0.000001))) fFill=false;
+				break;
+				}
+				default:
+				std::cout<<"error"<<std::endl;
+			}
+		
+
+			}//QE details
+		}//QE used
+			else{
+
+				std::cout<<"QE not used 431!!!"<<fUseQE<<std::endl;
+				 fFill=true;//no QE used
+
+				}
+			if(fFill==true) fCountOpDetDetected++;
+			if(fMakeDetectedPhotonsTree&&fFill==true) {
+std::cout<<"filling tree %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% details of QE "<<fDetailedQE<<" "<<fWavelength<<std::endl;
+				fThePhotonTreeDetected->Fill();
+				
+				}
+			
+			
+		      }//check for passing wavelength cut
+			fFill=false;
+		  }//simphotons loop
+	      }//verbosity <=3
 	  
-	  	      
+	  	std::cout<<"--------------------photons hitting PMTS "<<allphotons1<<" "<<allphotons2<<std::endl;      
 	    // If this is a library building job, fill relevant entry
 	    art::ServiceHandle<phot::PhotonVisibilityService> pvs;
 	    if(pvs->IsBuildJob())
 	      {
 		int VoxID; double NProd;
 		pvs->RetrieveLightProd(VoxID, NProd);
-		pvs->SetLibraryEntry(VoxID, fOpChannel, double(fCountOpDetAll)/NProd);		
+
+//change!!!!!
+		pvs->SetLibraryEntry(VoxID, fOpChannel, double(fCountOpDetDetected)/NProd);		
 	      }
 
 
 	    // Incremenent per event and fill Per OpDet trees	    
-	    if(fMakeOpDetsTree) fTheOpDetTree->Fill();
+
 	    fCountEventAll+=fCountOpDetAll;
 	    fCountEventDetected+=fCountOpDetDetected;
 
+	    std::cout<<"detected photons---------------------- "<<fCountOpDetDetected<<" for all event "<<fCountEventDetected<<std::endl;
+		if(fMakeOpDetsTree) fTheOpDetTree->Fill();
+
+
 	    // Give per OpDet output
-	    if(fVerbosity >2) std::cout<<"OpDetResponse PerOpDet : Event "<<fEventID<<" OpDet " << fOpChannel << " All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 
+	    if(fVerbosity >2) ;//std::cout<<"OpDetResponse PerOpDet : Event "<<fEventID<<" OpDet " << fOpChannel << " All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 
 	  }
 
 	// Fill per event tree
 	if(fMakeOpDetEventsTree) fTheEventTree->Fill();
 
 	// Give per event output
-	if(fVerbosity >1) std::cout<<"OpDetResponse PerEvent : Event "<<fEventID<<" All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 	
+	if(fVerbosity >1) ;//std::cout<<"OpDetResponse PerEvent : Event "<<fEventID<<" All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 	
 
       }
     else
@@ -330,6 +501,10 @@ namespace opdet {
 	// add an empty record to the per event tree 
 	if(fMakeOpDetEventsTree) fTheEventTree->Fill();
       }
+
+    //Reset counters
+   fCountOpDetAll=0;
+    fCountOpDetDetected=0;
     
     }
     else
@@ -337,13 +512,13 @@ namespace opdet {
     //Get SimPhotonsLite from Event
     art::Handle< std::vector<sim::SimPhotonsLite> > photonHandle; 
     evt.getByLabel("largeant", photonHandle);
-
+std::cout<<"===================================================photons lite !!!!!!! "<<std::endl;
     
     //Reset counters
     fCountEventAll=0;
     fCountEventDetected=0;
 
-    if(fVerbosity > 0) std::cout<<"Found OpDet hit collection of size "<< (*photonHandle).size()<<std::endl;
+    if(fVerbosity > 0) std::cout<<"Lite photons - Found OpDet hit collection of size "<< (*photonHandle).size()<<std::endl;
 
     
     if((*photonHandle).size()>0)
@@ -367,7 +542,7 @@ namespace opdet {
 
 		    //Get arrival time from phot
 		    fTime= it->first*2;
-		    std::cout<<"Arrival time: " << fTime<<std::endl;
+		    std::cout<<"491 Arrival time: " << fTime<<std::endl;
 		   
             for(int i = 0; i < it->second ; i++)
             {
@@ -380,8 +555,8 @@ namespace opdet {
 			fCountOpDetDetected++;
 			std::cout<<"OpDetResponse PerPhoton : Event "<<fEventID<<" OpChannel " <<fOpChannel << " Wavelength " << fWavelength << " Detected 1 "<<std::endl;
 		      }
-		    else
-		      std::cout<<"OpDetResponse PerPhoton : Event "<<fEventID<<" OpChannel " <<fOpChannel << " Wavelength " << fWavelength << " Detected 0 "<<std::endl;
+		    else;
+		    //  std::cout<<"OpDetResponse PerPhoton : Event "<<fEventID<<" OpChannel " <<fOpChannel << " Wavelength " << fWavelength << " Detected 0 "<<std::endl;
             }
             }
 	      }
@@ -413,13 +588,13 @@ namespace opdet {
 	    fCountEventDetected+=fCountOpDetDetected;
 
 	    // Give per OpDet output
-	    if(fVerbosity >2) std::cout<<"OpDetResponse PerOpDet : Event "<<fEventID<<" OpDet " << fOpChannel << " All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 
+	    if(fVerbosity >2) ;//std::cout<<"OpDetResponse PerOpDet : Event "<<fEventID<<" OpDet " << fOpChannel << " All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 
         }
         // Fill per event tree
         if(fMakeOpDetEventsTree) fTheEventTree->Fill();
 
         // Give per event output
-        if(fVerbosity >1) std::cout<<"OpDetResponse PerEvent : Event "<<fEventID<<" All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 	
+        if(fVerbosity >1) ;//std::cout<<"OpDetResponse PerEvent : Event "<<fEventID<<" All " << fCountOpDetAll << " Det " <<fCountOpDetDetected<<std::endl; 	
 
       }
     else
@@ -429,7 +604,12 @@ namespace opdet {
       if(fMakeOpDetEventsTree) fTheEventTree->Fill();
     } 
     }
+//if(QEHmm) delete QEHmm;
+//if(QEEtl) delete QEEtl;
+//if(QeEnergyHist) delete QeEnergyHist;
+//if(QESipm) delete QESipm;
   }
+
 }
 
 
